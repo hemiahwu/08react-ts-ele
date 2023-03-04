7345e86cb37df8ff5b08510717e25a6632f32ee3
吴海洋 <why@vip.local>
2023-03-04 21:47:54 +0800

    02

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Login from "./pages/login/Login";
+function App() {
+  return (
+    <main id="app">
+      <Login />
+    </main>
+  );
+}
+
+export default App;
